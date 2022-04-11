14c6e3ca7a222b8c0abb74de13860bb382f83758
Simo Puustinen <spuustin@c2r1p1.hive.fi>
2022-04-11 12:26:05 +0300

    fixed validation

--- validation.c
+++ validation.c
@@ -6,7 +6,7 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 12:17:28 by spuustin          #+#    #+#             */
-/*   Updated: 2022/04/11 12:20:29 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/04/11 12:25:54 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int	is_valid_prechar(char c)
 {
 	if (c == '-' || c == '.' || c == 'l' || c == 'h' || c == '#' \
-	 || (c >= '0' && c <= '9' ) || c == ' ' || c == '+' || c == '-')
+	|| (c >= '0' && c <= '9' ) || c == ' ' || c == '+' || c == '-')
 		return (1);
 	return (0);
 }
@@ -25,13 +25,13 @@ int	is_valid_prechar(char c)
 void	octal_or_hexal(t_build *b, char c)
 {
 	if (c == 'x' || c == 'X')
-			b->base = 16;
-		if (c == 'o')
-		{
-			b->base = 8;
-			if (b->hashtag == 1)
-				b->prefix = 1;
-		}
-		b->space = 0;
-		b->plus = 0;
+		b->base = 16;
+	if (c == 'o')
+	{
+		b->base = 8;
+		if (b->hashtag == 1)
+			b->prefix = 1;
+	}
+	b->space = 0;
+	b->plus = 0;
 }
